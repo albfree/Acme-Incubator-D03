2a814e1406f5e2124531cf1d8547a4ab74d90acf
brapercob <brayanpercob@gmail.com>
2020-07-28 20:16:19 +0200

    Añadido update funcional de customization

--- src/main/java/acme/features/administrator/customization/AdministratorCustomizationUpdateService.java
+++ src/main/java/acme/features/administrator/customization/AdministratorCustomizationUpdateService.java
@@ -5,16 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.customizations.Customization;
+import acme.framework.components.Errors;
 import acme.framework.components.Model;
 import acme.framework.components.Request;
 import acme.framework.entities.Administrator;
-import acme.framework.services.AbstractShowService;
+import acme.framework.services.AbstractUpdateService;
 
 @Service
-public class AdministratorCustomizationShowService implements AbstractShowService<Administrator, Customization> {
+public class AdministratorCustomizationUpdateService implements AbstractUpdateService<Administrator, Customization> {
 
 	@Autowired
-	private AdministratorCustomizationRepository repository;
+	AdministratorCustomizationRepository repository;
 
 
 	@Override
@@ -22,6 +23,15 @@ public class AdministratorCustomizationShowService implements AbstractShowServic
 		assert request != null;
 
 		return true;
+	}
+
+	@Override
+	public void bind(final Request<Customization> request, final Customization entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		request.bind(entity, errors);
 	}
 
 	@Override
@@ -45,6 +55,22 @@ public class AdministratorCustomizationShowService implements AbstractShowServic
 		result = this.repository.findOneById(id);
 
 		return result;
+	}
+
+	@Override
+	public void validate(final Request<Customization> request, final Customization entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+	}
+
+	@Override
+	public void update(final Request<Customization> request, final Customization entity) {
+		assert request != null;
+		assert entity != null;
+
+		this.repository.save(entity);
 	}
 
 }
--- src/main/webapp/META-INF/views/administrator/customization/form.jsp
+++ src/main/webapp/META-INF/views/administrator/customization/form.jsp
@@ -4,10 +4,12 @@
 <%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@taglib prefix="acme" tagdir="/WEB-INF/tags"%>
 
-<acme:form readonly="true">
-	<acme:form-textarea code="administrator.customization.form.label.spam-words" path="spamWords" />
-	<acme:form-textbox code="administrator.customization.form.label.threshold" path="threshold" />
-	<acme:form-textarea code="administrator.customization.form.label.activity-sectors" path="activitySectors" />
-		
-  	<acme:form-return code="administrator.customization.form.button.return"/>
+<acme:form>
+	<acme:form-textarea code="administrator.customization.form.label.spam-words" path="spamWords"/>
+	<acme:form-double code="administrator.customization.form.label.threshold" path="threshold"/>
+	<acme:form-textarea code="administrator.customization.form.label.activity-sectors" path="activitySectors"/>
+
+	<acme:form-submit code="administrator.customization.form.button.update" action="/administrator/customization/update" />
+
+	<acme:form-return code="administrator.customization.form.button.return" />
 </acme:form>
